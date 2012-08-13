ea0ad9df6a18290b1e5838ebf0107fda29781e70
czatup <czat.up@gmail.com>
2012-08-13 11:30:25 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'c0bbe0d9925a3b80580c7799985886aecb253bcbec4d34e4d8cbe6355dda6b4212f85f0824c019c308cd5b7ed18b7602971d8ca250c200590d2910c5379ec5be'
